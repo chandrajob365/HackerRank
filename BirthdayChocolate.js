a471ef58f7441d7c4e33c6cc1fa89850c56a8255
Manish <chandrajob365@gmail.com>
2017-10-24 16:00:11 +0530

    Update to run arr.length -m times only

--- BirthdayChocolate.js
+++ BirthdayChocolate.js
@@ -1,6 +1,6 @@
-let chocoPies = [4]
+let chocoPies = [1, 2, 1, 3, 2]
 const solve = (arr, m, d, count) => {
-  if (arr.length === 0) return count
+  if (arr.length < m) return count
   if ((arr[0] <= d) && isValidSubSeq(arr.slice(0, m), d)) {
     count++
   }
@@ -11,4 +11,4 @@ const isValidSubSeq = (arr, d) => {
   return (arr.reduce((acc, elm) => acc + elm) === d)
 }
 
-console.log(solve(chocoPies, 1, 4, 0))
+console.log(solve(chocoPies, 2, 3, 0))
